325134abe52bda996b0347146982f75f22e15ad0
Partha-UDS <partha.das@german-uds.de>
2025-06-04 17:25:14 +0200

    correctly set the route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/foundation-website',
+  assetPrefix: '/foundation-website/',
+  trailingSlash: true,
   async headers() {
     return [
       {
