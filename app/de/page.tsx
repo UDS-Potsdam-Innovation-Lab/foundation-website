5e1a16717ed534eda83c998e5de3e0fd107570f4
Partha-UDS <partha.das@german-uds.de>
2025-06-17 14:09:16 +0200

    wip - translation

--- app/de/page.tsx
+++ app/de/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
-import ParticlesBackground from './components/ParticlesBackground';
+import ParticlesBackground from '../components/ParticlesBackground';
 
 export default function Home() {
   return (
